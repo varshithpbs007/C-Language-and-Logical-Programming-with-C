0a382a8d122accd6ff0e4a3ae1aca951af5936f9
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-11-07 16:14:39 +0530

    Update C language important practice programs.c

--- C language important practice programs.c
+++ C language important practice programs.c
@@ -1,1 +1,18 @@
+1. //WAP to check if a number is even or odd
+#include<stdio.h>
+void main(){
+    int n;
+    printf("Enter the number :");
+    scanf("%d",&n);
+    if(n % 2 == 0) // % is the remainder operator
+    {
+        printf("The number %d is even",n);
+    }
+    else
+    {
+        printf("The number %d is odd",n);
+    }
+}
 
+
+2.
