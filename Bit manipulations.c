279d793dce6041878a9ab112032dd7841f34f705
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-11-09 18:27:56 +0530

    Update Bit manipulations.c

--- Bit manipulations.c
+++ Bit manipulations.c
@@ -27,6 +27,29 @@ void main()
 
 
 
-// 2.
+// 2. /* Program to check number of set bits (1's) in binary representation of the inputted number by the user.
+---> Logic used used is we " to clear the right most set bit in n using n & (n-1) " and count how many times we had to clear right most bit until n becomes 0. */
+/* Brian Kernighan's algorthm */
+
+#include<stdio.h>
+int main()
+{
+    int n,count = 0;
+    
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    
+    while(n != 0)
+    {
+        n = n & (n-1);
+        count++;
+    }
+    
+    printf("No of set bits (1's) in the entered number is = %d",count);
+}
+
+
+
+// 3. 
 
 
