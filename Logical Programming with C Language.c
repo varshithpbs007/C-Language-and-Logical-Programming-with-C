6fbde4cce3cc317ef1470e0281570d2308feb599
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-11-18 18:46:48 +0530

    Update Logical Programming with C Language.c

--- Logical Programming with C Language.c
+++ Logical Programming with C Language.c
@@ -137,4 +137,23 @@ int main() {
 }
 
 
-6.
+6. /*-------- Biggest of 3 integers using conditional operator ( ternary operator ) ------ */
+
+#include <stdio.h>
+int main(){
+    
+    int a,b,c;
+    int largest;
+    printf("Enter 3 integers:");
+    scanf("%d %d %d",&a,&b,&c);
+    
+     largest = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+     printf("Largest number is %d",largest); 
+    
+    /* a > b && a > c ? printf(" a = %d is largest",a) : b > c ? printf(" b = %d is largest",b) : printf(" c = %d is largest",c); */
+    
+    return 0;
+}
+
+
+7. 
