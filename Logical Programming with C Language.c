7be2435678fdf819640febd784129b41d23dfc43
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-11-22 11:36:40 +0530

    Update Logical Programming with C Language.c

--- Logical Programming with C Language.c
+++ Logical Programming with C Language.c
@@ -291,4 +291,24 @@ int main() {
 }
 
 
-12. 
+12. /* ----- To display the multiplication table for the user inputted number
+   ----- */
+#include <stdio.h>
+
+int main() {
+    
+    int n,i;
+    printf("Enter a number:\n");
+    scanf("%d",&n);
+    
+    for(i=1 ; i <= 10 ; i++)
+    {
+        printf("%d * %d = %d\n",n,i,n*i);
+    }
+    
+    return 0;
+    
+}
+
+
+13. 
