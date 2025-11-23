56637c18f83893af5875cd9a63072bc3983676c9
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-11-23 20:56:26 +0530

    Update Logical Programming with C Language.c

--- Logical Programming with C Language.c
+++ Logical Programming with C Language.c
@@ -504,4 +504,31 @@ int main() {
 }
 
 
-20. 
+20. /* Sum of digits in the user inputted number----- */
+
+#include <stdio.h>
+
+int main() {
+    
+    int n,rem=0,sum=0;
+    printf("Enter a number to find sum of digits:");
+    scanf("%d",&n);
+    
+    if(n == 0)
+    {
+        printf("Sum of digits in entered number is 0");
+        return 0;
+    }
+    while(n>0)
+    {
+        rem = n % 10;
+        sum = sum + rem;
+        n = n / 10;
+    }
+    
+    printf("Sum of digits in entered number is %d",sum);
+    return 0;
+}
+
+
+21. 
